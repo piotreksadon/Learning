582c1cc26fcb7f7d680e362095bc43118fb99795
Sadek <piotrek.sadon@gmail.com>
2020-11-30 14:29:05 +0100

    switchLearning

---
+++ index2.js
@@ -0,0 +1,22 @@
+const day = 'środa';
+
+switch (day) {
+    case 'poniedziałek':
+        console.log('Dzisiaj jest poniedziałek.')
+        break;
+    case 'wtorek':
+        console.log('Dzisiaj jest wtorek.')
+        break;
+    case 'środa':
+        console.log('Dzisiaj jest środa.')
+        break;
+    case 'czwartek':
+        console.log('Dzisiaj jest czwartek.')
+        break;
+    case 'piątek':
+        console.log('Dzisiaj jest piątek.')
+        break;
+    default:
+        console.log('Dzisiaj jest weekend!')
+
+};
