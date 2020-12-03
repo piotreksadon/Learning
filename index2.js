d5f13c44016691442571c0d36a77cae9e794cc46
Sadek <piotrek.sadon@gmail.com>
2020-12-03 16:00:30 +0100

    Nauka na dzien 03.12.2020.

--- index2.js
+++ index2.js
@@ -1,22 +1,136 @@
-const day = 'środa';
+// const day = 'środa';
 
-switch (day) {
-    case 'poniedziałek':
-        console.log('Dzisiaj jest poniedziałek.')
-        break;
-    case 'wtorek':
-        console.log('Dzisiaj jest wtorek.')
-        break;
-    case 'środa':
-        console.log('Dzisiaj jest środa.')
-        break;
-    case 'czwartek':
-        console.log('Dzisiaj jest czwartek.')
-        break;
-    case 'piątek':
-        console.log('Dzisiaj jest piątek.')
-        break;
-    default:
-        console.log('Dzisiaj jest weekend!')
+// switch (day) {
+//     case 'poniedziałek':
+//         console.log('Dzisiaj jest poniedziałek.')
+//         break;
+//     case 'wtorek':
+//         console.log('Dzisiaj jest wtorek.')
+//         break;
+//     case 'środa':
+//         console.log('Dzisiaj jest środa.')
+//         break;
+//     case 'czwartek':
+//         console.log('Dzisiaj jest czwartek.')
+//         break;
+//     case 'piątek':
+//         console.log('Dzisiaj jest piątek.')
+//         break;
+//     default:
+//         console.log('Dzisiaj jest weekend!')
 
-};
+// };
+
+// const x = 10;
+
+// if (x > 20) {
+//     console.log('x większy niż 20')
+// } else {
+//     console.log('x mniejszy niż 20')
+// };
+// console.log(      (x > 20) ? '[X większy niż 20]' : '[X jest mniejszy niż 20]')
+
+
+// const y = 20;
+// let $text;
+
+// if (y < 50) {
+//     $text = `${y} jest mniejszy niż 50`;
+// } else {
+//     $text - `${y} jest większy niż 50`;
+// }
+// console.log($text)
+
+
+// let txt = (y > 50) ? `${y} jest większy niż 50` : `${y} jest mniejszy niż 50`;
+
+// console.log(txt)
+
+
+// const x = 50;
+// const y = 30;
+// if (x >= y) {
+//     console.log('X większy od Y');
+// };
+
+
+// const color = 'blue';
+// const newColor = 'green';
+// if (color === newColor) {
+//     console.log('Kolor się zgadza.')
+// } else {
+//     console.log('Kolor się nie zgadza.')
+// };
+
+// const num1 = '50';
+// const num2 = 50;
+
+// if (num1 === num2) {
+//     console.log('OK');
+// } else {
+//     console.log('NIE OK');
+// };
+
+
+// const x = 100;
+// const y = 110;
+
+// if (x > y) {
+//     console.log('x jest większy od y.');
+// } else if (x === y) {
+//     console.log('x jest równy y');
+// } else {
+//     console.log('x nie jest większy od y');
+// };
+
+// const PROMO = '40%'
+// switch (PROMO) {
+//     case '10%':
+//         console.log('Dziś mamy 10% zniżki');
+//         break;
+
+//     case '20%':
+//         console.log('Dziś mamy 20% zniżki');
+//         break;
+
+//     case '30%':
+//         console.log('Dziś mamy 30% zniżki');
+//         break;
+    
+//     default:
+//         console.log('Nie ma zniżki.');
+// };
+
+
+// const x = 10;
+
+// // console.log((x%2 === 0) ? 'Jest podzielne.' : 'Nie jest podzielne.')
+// const check = (x%2 === 0) ? 'Jest podzielne.' : 'Nie jest podzielne.';
+// console.log(check);
+
+
+// let x = 50;
+// let $txt;
+
+// if (x >= 100){
+//     $txt = 'x jest większy.';
+// } else if (x < 100 && x > 30) {
+//     $txt = 'x jest średniakiem.';
+// } else {
+//     $txt = 'x jest mniejszy.';
+// };
+
+// console.log($txt.toUpperCase());
+
+
+
+//  for (let i = 0; i <4; i++) {
+//      console.log(i);
+//  };
+
+
+const animals = ['dog', 'cat', 'bird', 'cow', 'whale'];
+
+for (let i = 0; i <5; i++) {
+    console.log(animals[i])
+}
